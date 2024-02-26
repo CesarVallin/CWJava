e0f89cd1afc5e320cd9e693e82052358cc143a75
CesarVallin <cesarvallin.cv@gmail.com>
2024-02-26 12:02:08 -0600

    add function

--- src/cw/HelloWorld.java
+++ src/cw/HelloWorld.java
@@ -5,10 +5,16 @@ public class HelloWorld {
 	public static String sayHello(String s) {
 		return "Hello " + s;
 	}
+	
+	public static int addTwo(int number) {
+		return number + 2;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(HelloWorld.sayHello("Testing this one"));
+		System.out.println(HelloWorld.addTwo(2));
+
 	}
 
 }
