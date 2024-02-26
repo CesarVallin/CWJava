976bf7090b559f3e5723f322b24e46ff6b7804a4
CesarVallin <cesarvallin.cv@gmail.com>
2024-02-26 11:54:25 -0600

    first commit

---
+++ src/cw/HelloWorld.java
@@ -0,0 +1,14 @@
+package cw;
+
+public class HelloWorld {
+	
+	public static String sayHello(String s) {
+		return "Hello " + s;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(HelloWorld.sayHello("Testing this one"));
+	}
+
+}
