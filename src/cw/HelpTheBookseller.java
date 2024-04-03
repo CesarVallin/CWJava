233644a174c09f64c29004cc9a7b3944813d9a6d
CesarVallin <cesarvallin.cv@gmail.com>
2024-04-02 23:27:41 -0500

    HelpTheBookseller continue

--- src/cw/HelpTheBookseller.java
+++ src/cw/HelpTheBookseller.java
@@ -30,18 +30,14 @@ public class HelpTheBookseller {
 			// access first character - of each string - in the first string array. 
 			char c = lstOfArt[i].charAt(0); // method call on first element of string. 
 			
-			// access numeric value - of each string - in the first string array. 
-			int quantity = Integer.parseInt(lstOfArt[i].replaceAll("[^0-9]", "")); // reference: https://www.geeksforgeeks.org/extract-all-integers-from-the-given-string-in-java/
+			// Variable quantity should be declared and initialized with a value of 0.
+			int quantity = 0; 
 			
 			// now iterate through other array
 			for (int j = 0; j < lstOf1stLetter.length; j ++) {
-				// CHECK IF c or d ARE EMPTY, RETURN AN EMPTY String. ---  ---  ---  ---  ---  ---  ---  --- `c & d respectively with array interpretation`
-				if (lstOf1stLetter.length == 0) {
-					return "";
-				}
 				
 				// Ready each element from string to char 
-				char d = lstOf1stLetter[j].charAt(j); // method call on each element of string. 
+				char d = lstOf1stLetter[j].charAt(j); // method call on each element of string. ---.charAt(j) might not be needed--- 
 				
 				// CHECK LAST ITERATION - off scenario - meet this scenario first!!
 				// ------------------------------------------------------------------
@@ -49,13 +45,13 @@ public class HelpTheBookseller {
 				
 				// Conditional & utilize `quantity` variable. 
 				if (c == d) {
-					//
-					quantity += Integer.parseInt(lstOfArt[i]); // add the number value from initial array iteration.
+					// access numeric value - of each string - in the first string array. 
+					quantity += Integer.parseInt(lstOfArt[i].replaceAll("[^0-9]", "")); // reference: https://www.geeksforgeeks.org/extract-all-integers-from-the-given-string-in-java/
 				}
-				construction.append("(" + d + " : " + quantity + ")");
-				result.append(construction);
-				
 			}
+			
+			construction.append("(" + c + " : " + quantity + ")");
+			result.append(construction);
 		}
 		
 		return result.toString();
