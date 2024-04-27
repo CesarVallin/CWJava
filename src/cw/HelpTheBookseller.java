6a17962e7fcb78acaf01f168af2f31669e96c7e1
CesarVallin <cesarvallin.cv@gmail.com>
2024-04-27 03:02:02 -0500

    HashMap intergrated; more work to do

--- src/cw/HelpTheBookseller.java
+++ src/cw/HelpTheBookseller.java
@@ -1,54 +1,52 @@
 package cw;
 
+import java.util.HashMap;
+
 public class HelpTheBookseller {
 	
 	// values to test...
 	public static String art[] = new String [] {"ABAR 200", "CDXE 500", "BKWR 250", "BTSQ 890", "DRTY 600"};
 	public static String cd[] = new String [] {"A", "B"};
-	// ...
-	
 	
 	// cw `Help the bookseller !`
 	
 	// 1st parameter is the stocklist (L in example), 
 	// 2nd parameter is list of categories (M in example)
 	// Your task is to find all the books of L with codes belonging to each category of M and to sum their quantity according to each category.
+	
 	public static String stockSummary(String lstOfArt[], String lstOf1stLetter[]) {
-		
+		// Declare and initialize a HashMap - data structure
+		HashMap<Character, Integer> charMap = new HashMap<>();	
 		// Declare and initiate a StringBuilder - remember, in java, Strings are immutable.
-		StringBuilder result = new StringBuilder(); // call of StringBuilder method. 
-		
+		StringBuilder result = new StringBuilder(); // call of StringBuilder method. 	
 		// Check if lstOfArt or lstOf1stLetter is empty
 	    if(lstOfArt.length == 0 || lstOf1stLetter.length == 0) {
 	        return "";
 	    }
-		
-		// iterate through the first array
+		// iterate through the first array --- populate HashMap
 		for (int i = 0; i < lstOfArt.length; i++) {
-			// Base construction
-			StringBuilder construction = new StringBuilder(); // construction `tool` to help you build the `result` String.		
-			// access first character - of each string - in the first string array. 
 			char c = lstOfArt[i].charAt(0); // method call on first element of string.  String class charAt() method returns a char value.  
-			
-			// Variable quantity should be declared and initialized with a value of 0.
-			int quantity = 0; 		
-			// now iterate through other array
-			for (int j = 0; j < lstOf1stLetter.length; j ++) {			
-				// Ready each element from string to char 
-				char d = lstOf1stLetter[j].charAt(0); // method call on each element of string.
-				// More notes on this available!
+			// Check to see if the char can be added to HashMap
+			if(!charMap.containsKey(c)) {
+				// Variable quantity should be declared and initialized with a value of 0.
+				int quantity = 0; 			
+				// now iterate through other array
+				for (int j = 0; j < lstOf1stLetter.length; j ++) {			
+					// Ready each element from string to char 
+					char d = lstOf1stLetter[j].charAt(0); // method call on each element of string.
+							// More notes on this available!
+					// Conditional & utilize `quantity` variable. 
+					if (c == d) {
+						// access numeric value - of each string - in the first string array. 
+						quantity += Integer.parseInt(lstOfArt[i].replaceAll("[^0-9]", "")); // reference: https://www.geeksforgeeks.org/extract-all-integers-from-the-given-string-in-java/
+					}
+					// SOLVED FOR values that did not add anything!!
+					if(quantity == 0) {
+						continue;
+					}
 					
-				// Conditional & utilize `quantity` variable. 
-				if (c == d) {
-					// access numeric value - of each string - in the first string array. 
-					quantity += Integer.parseInt(lstOfArt[i].replaceAll("[^0-9]", "")); // reference: https://www.geeksforgeeks.org/extract-all-integers-from-the-given-string-in-java/
 				}
-			}		
-			// SOLVED FOR values that did not add anything!!
-			if(quantity == 0) {
-				continue;
-			}
-			
+			}				
 			construction.append("(" + c + " : " + quantity + ")");
 			result.append(construction);
 			
