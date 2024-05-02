6dc3c78c892be7cec5c29a0bb54fc11b67dde6c9
CesarVallin <cesarvallin.cv@gmail.com>
2024-05-01 23:45:00 -0500

    HashMap progress on HelpTheBookseller

--- src/cw/HelpTheBookseller.java
+++ src/cw/HelpTheBookseller.java
@@ -25,7 +25,7 @@ public class HelpTheBookseller {
 	    }
 		// iterate through the first array --- populate HashMap
 		for (int i = 0; i < lstOfArt.length; i++) {
-			char c = lstOfArt[i].charAt(0); // method call on first element of string.  String class charAt() method returns a char value.  
+			char c = lstOfArt[i].charAt(0); // method call on the first element of each string.  String class charAt() method returns a char value.  
 			// Check to see if the char can be added to HashMap
 			if(!charMap.containsKey(c)) {
 				// Variable quantity should be declared and initialized with a value of 0.
@@ -33,19 +33,22 @@ public class HelpTheBookseller {
 				// now iterate through other array
 				for (int j = 0; j < lstOf1stLetter.length; j ++) {			
 					// Ready each element from string to char 
-					char d = lstOf1stLetter[j].charAt(0); // method call on each element of string.  String class charAt() method returns a char value. 
+					char d = lstOf1stLetter[j].charAt(0); // method call on string element.  String class charAt() method returns a char value. 
 					// Conditional & utilize `quantity` variable. 
 					if (c == d) {
 						// access numeric value - of each string - in the first string array. 
 						quantity += Integer.parseInt(lstOfArt[i].replaceAll("[^0-9]", "")); // reference: https://www.geeksforgeeks.org/extract-all-integers-from-the-given-string-in-java/
 					}
-					// SOLVED FOR values that did not add anything!!
-					if(quantity == 0) {
-						continue;
-					}
-					
+					charMap.put(d, quantity);
 				}
-			}				
+			}
+			if(charMap.containsKey(c)) {
+				// Get the value from the current existing key
+				int quantity = 
+			}
+				
+				
+				
 			construction.append("(" + c + " : " + quantity + ")");
 			result.append(construction);
 			
