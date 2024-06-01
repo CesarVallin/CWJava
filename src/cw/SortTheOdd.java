a8fc8b6bb6966434641d8e43243a3e9dcc1e2bdc
CesarVallin <cesarvallin.cv@gmail.com>
2024-05-31 23:48:06 -0500

    SortTheOdd Class with classes within

---
+++ src/cw/SortTheOdd.java
@@ -0,0 +1,28 @@
+package cw;
+
+public class SortTheOdd {
+	
+	// private class for SortTheOdd
+	// Tools
+	private class Tools {
+		// Create a `blank` stock array
+		static int[] stockArray;
+		
+	}
+	
+	public static int[] sortArray(int[] array) {
+		
+		// `blank` array has the same length as `array` parameter
+		Tools.stockArray = new int[array.length];
+		// Loop through array parameter
+		for (int i = 0; i < array.length; i++) {
+			// conditional for even number
+			if(array[i] % 2 == 0) {
+				Tools.stockArray[i] = i;
+			}
+		}
+		
+		return array;
+	}
+
+}
