63d21d96965b1a84cbeb058d893196810b8ad9b8
CesarVallin <cesarvallin.cv@gmail.com>
2024-06-10 14:09:46 -0500

    ready for first test, SortTheOdd

--- src/cw/SortTheOdd.java
+++ src/cw/SortTheOdd.java
@@ -14,8 +14,13 @@ public class SortTheOdd {
 		static void evenInteger(int i, int num) {
 			stockArray[i] = num;
 		}
+		// void method to `modify` stockArray - odd numbers
+		static void oddInteger(int i, int num) {
+			stockArray[i] = num;
+		}
 		
-		// method to return only odd numbers, sorted
+		
+		// method to return only odd numbers, sorted, ready for comparison
 		static ArrayList<Integer>oddIntegers(int[] arrayPlaceholder) {
 			ArrayList<Integer> odds = new ArrayList<Integer>();
 			for (int j = 0; j < arrayPlaceholder.length; j++) {
@@ -32,6 +37,9 @@ public class SortTheOdd {
 		
 		// Tools class - `no-modifications` array now has the same length as `array` parameter - with all values initialized to zero. 
 		Tools.stockArray = new int[array.length];
+		
+		// Tools class - `ordered odds only` - based on `int[] array` parameter
+		ArrayList<Integer> ascendingOdds = Tools.oddIntegers(Tools.stockArray);
 		// Loop through array parameter
 		for (int i = 0; i < array.length; i++) {
 	
@@ -40,14 +48,15 @@ public class SortTheOdd {
 				// Tools class - even Integer
 				Tools.evenInteger(i, array[i]);
 			}
-			// Just switch them! criss-cross apple sauce
-			// check for the lesser number ahead
+			// conditional for odd number - checking through a sorted array list
 			if(array[i] % 2 != 0) {
-				
+				for (int j = 0; j < ascendingOdds.size(); j++) {
+					Tools.oddInteger(j, ascendingOdds.get(j));
+				}
 			}
 		}
 		
-		return array;
+		return Tools.stockArray;
 	}
 
 }
