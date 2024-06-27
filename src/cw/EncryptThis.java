1e2ebfdc85dea737aafd0c039c8dd618f173ba1c
CesarVallin <cesarvallin.cv@gmail.com>
2024-06-27 15:19:28 -0500

    initial set up of EncryptThis

---
+++ src/cw/EncryptThis.java
@@ -0,0 +1,18 @@
+package cw;
+
+public class EncryptThis {
+	
+	// Tools pack class
+	private class Tools {
+		// find ascii value of character
+		static int asciiValue(char character) {
+			return (int) character;
+		}
+	}
+	
+	// Main
+	public static String encryptThis(String text) {
+		return "";
+	}
+
+}
