827feff74dc6d0f68921aca9b37113be2cb84557
CesarVallin <cesarvallin.cv@gmail.com>
2024-07-05 18:41:56 -0500

    ready to test

--- src/cw/EnoughIsEnough.java
+++ src/cw/EnoughIsEnough.java
@@ -7,11 +7,10 @@ public class EnoughIsEnough {
 	// Tools pack class
 	private class Tools {
 		
-		// field to populate array with a maxOccurrences number
-		static ArrayList<Integer> maxOccurrList = new ArrayList<>();
-		
-		// void method to actually populate array list
-		static void loopAndAssignMaxOccur(int[] arrayParam, int maxOccurences) {
+		// ArrayList method to actually populate array list
+		static ArrayList<Integer> loopAndPopulateArrayList(int[] arrayParam, int maxOccurences) {
+			// field to populate array with a maxOccurrences number
+			ArrayList<Integer> maxOccurrList = new ArrayList<>();
 			for (int i = 0; i < arrayParam.length; i++) {
 				// Start occurence counter
 				int occurenceCounter = 0;
@@ -19,25 +18,37 @@ public class EnoughIsEnough {
 				for (int j = 0; j < arrayParam.length; j++) {
 					if (arrayParam[i] == arrayParam[j]) {
 						occurenceCounter ++;
-					}
-					
+					}				
 				}
 				// conditional outside of nested loop to make a decision to include integer in array list or not based on comparator
 				if (occurenceCounter <= maxOccurences) {
 					// add current index
-					Tools.maxOccurrList.add(arrayParam[i]);
+					maxOccurrList.add(arrayParam[i]);
 				}
 			}
+			return maxOccurrList;
 		}
 		
+		// int[] array method to loop through populated array list and populate int[] array
+		// Convert ArrayList to array --------------------------------------------------------------
+        // Declare and initialize array with number of indices - Specify data type and number of elements. 
+        // Assign a value to each index. 
+		static int[] loopAndPopulateArray(ArrayList<Integer> arrayListParam) {
+			int[] regularArray = new int[arrayListParam.size()];
+			// Loop through array list, and assign to regular array
+			for (int i = 0; i < arrayListParam.size(); i++) {
+				regularArray[i] = arrayListParam.get(i);
+			}
+			return regularArray;
+		}
 	}
 	
 	public static int[] deleteNth(int[] elements, int maxOccurrences) {
 		
-		ArrayList<Integer> finalAnswer = 
+		ArrayList<Integer> finalAnswerList = Tools.loopAndPopulateArrayList(elements, maxOccurrences);
+		int[] finalAnswerArray = Tools.loopAndPopulateArray(finalAnswerList);
 		
-		
-		return null;
+		return finalAnswerArray;
 	}
 
 }
