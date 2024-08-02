7c76e9b39e7aa35441da7df31178d85af2607209
CesarVallin <cesarvallin.cv@gmail.com>
2024-08-02 15:56:48 -0500

    Plan for next methods.

--- src/cw/SupermarketQueue.java
+++ src/cw/SupermarketQueue.java
@@ -13,9 +13,24 @@ public class SupermarketQueue {
 			}
 			return total;
 		}
+		
+		// Check for greater number in Queue based on Tills n
+		// Add that number to a time variable. 
+		// Subtract lesser number(s) that made it through the first comparison 
+		// If there is still a greater number, subtract from it again, otherwise, new number(s) becomes greatest, and new greatest will be subtracted by old greatest. 
+		
+		// Keep looping.  
+		
+		
 	}
 	
 	public static int SolveMarketQueue(int [] customers, int n) {
+		// Check for `one` till open and up all customer time
+		if (n == 1) {
+			return Tools.checkForOneTillOpen(customers);
+		}
+		
+		
 		return 0;
 	}
 
